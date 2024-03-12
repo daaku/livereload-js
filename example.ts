f09a640ca4018a02e6b04a5277c5862c5c0cceee
Naitik Shah <n@daaku.org>
2024-03-12 16:10:34 +0400

    fix example

--- example.ts
+++ example.ts
@@ -1,14 +1,14 @@
-import { liveReloadHandler, liveReloadJS } from './index'
+import { lrPath, lrJS, lrHandler } from './index'
 
 Bun.serve({
   fetch(req: Request) {
-    if (new URL(req.url).pathname === '/livereload') {
-      return liveReloadHandler(req)
+    if (new URL(req.url).pathname === lrPath) {
+      return lrHandler(req)
     }
     return new Response(
       `<!doctype html>
       <strong>Hello, world</strong>
-      <script>${liveReloadJS}</script>`,
+      <script>${lrJS}</script>`,
       { status: 200, headers: { 'Content-Type': 'text/html' } },
     )
   },
